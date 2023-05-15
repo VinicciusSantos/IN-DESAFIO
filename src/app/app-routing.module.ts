f9afca2a7d056698615580e4bc53d23a764c87a9
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2023-05-15 15:25:00 -0300

    :sparkles: creating first part of layout 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-export const mainRoutes: Routes = [];
-
+const mainRoutes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then(m => m.HomeModule),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(mainRoutes, { useHash: true })],
   exports: [RouterModule],
